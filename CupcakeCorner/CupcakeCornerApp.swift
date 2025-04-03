c97219d827fa68833c5a8b5fbc6bd8d741fcf423
seahpark247 <famousjedo@gmail.com>
2025-04-03 13:27:01 -0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Seah Park on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

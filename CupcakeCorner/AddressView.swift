48ce7b3ee1e538479b38ff8ac99192a4cb15105b
seahpark247 <famousjedo@gmail.com>
2025-04-06 21:06:47 -0500

    practice

--- CupcakeCorner/AddressView.swift
+++ CupcakeCorner/AddressView.swift
@@ -2,35 +2,17 @@
 //  AddressView.swift
 //  CupcakeCorner
 //
-//  Created by Seah Park on 4/3/25.
+//  Created by Seah Park on 4/6/25.
 //
 
 import SwiftUI
 
 struct AddressView: View {
-    @Bindable var order: Order
-
     var body: some View {
-        Form {
-            Section {
-                TextField("Name", text: $order.name)
-                TextField("Street Address", text: $order.streetAddress)
-                TextField("City", text: $order.city)
-                TextField("Zip", text: $order.zip)
-            }
-
-            Section {
-                NavigationLink("Check out") {
-                    CheckoutView(order: order)
-                }
-            }
-            .disabled(order.hasValidAddress == false)
-        }
-        .navigationTitle("Delivery details")
-        .navigationBarTitleDisplayMode(.inline)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    AddressView(order: Order())
+    AddressView()
 }
